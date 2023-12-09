27c1ecd6c9cd210d46ae9933c8039b8334c3ad12
zigol <zigolenko@rambler.ru>
2023-12-09 23:08:05 +0300

    PyTest is work

--- Bel.py
+++ Bel.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import unittest
-import pytest
-
+import pytest 
 
 def my_function(link):
     browser = webdriver.Chrome()
@@ -15,21 +14,19 @@ def my_function(link):
     input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Petrov")
 
     input3 = browser.find_element(By.CLASS_NAME, "form-control.third").send_keys("Petrov555")
-    
+
     button = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
-   
+
     return browser.find_element(By.TAG_NAME, "h1").text
-   
+
 
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
         link = "http://suninjuly.github.io/registration1.html"        
-        assert("Congratulations! You have successfully registered!", my_function(link), "Should be absolute value of a number")
+        self.assertEqual("Congratulations! You have successfully registered!", my_function(link), "Should be absolute value of a number")
 
     def test_abs2(self):
         link = "http://suninjuly.github.io/registration2.html"        
-        assert("Congratulations! You have successfully registered!", my_function(link), "Should be absolute value of a number")
+        self.assertEqual("Congratulations! You have successfully registered!", my_function(link), "Should be absolute value of a number")
 if __name__ == "__main__":
-    pytest.main()
-
-
+    unittest.main()
